bcdb02fe185449ffbe9471b64d5ebe77d7352d65
maxwell ramos <maxii.raamos.carol@gmail.com>
2022-05-05 00:12:59 -0300

    criacao do programa principal e utilizacao da classe Funcionario

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -9,11 +9,23 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-				
-		
-		
-		
-		
+		System.out.println("Nome do funcionario: ");
+		String nome = sc.nextLine();
+		System.out.println("salario Bruto R$: ");
+		double salarioBruto = sc.nextDouble();
+		System.out.println("Imposto cobrado no salario R$: ");
+		double imposto = sc.nextDouble();
+
+		// instanciação do objeto
+		Funcionario fc = new Funcionario(nome, salarioBruto, imposto);
+
+		System.out.println(fc);
+
+		System.out.println("digite o aumento do salario: (numero Real) R$ ");
+		double aumento = sc.nextDouble();
+		fc.aumento(aumento);
+		System.out.println(fc);
+
 		sc.close();
 
 	}
